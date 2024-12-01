a38be1afb423efb15d24e3f730f06373de1727f3
Nikolly Cardoso de Faria <nikolly@syncode.com.br>
2024-12-01 20:58:58 -0300

    Alterações no Dockerfile

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
-import tensorflow as tf
-
 
 app = FastAPI(title="Finance API", description="API para coletar dados e treinar um modeelo LSTM.")
 
